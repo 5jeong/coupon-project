c34a5932ce3c65b2a19749bf4c20f5eb04822c8f
5jeong <wjdgns4385@naver.com>
2025-01-20 18:06:47 +0900

    ✨ [feat] 쿠폰 엔티티 작성

---
+++ coupon-core/src/main/java/com/example/couponcore/model/Coupon.java
@@ -0,0 +1,50 @@
+package com.example.couponcore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "coupons")
+public class Coupon extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    CouponType couponType;
+
+    private Integer totalQuantity;
+
+    @Column(nullable = false)
+    private int issuedQuantity;
+
+    @Column(nullable = false)
+    private int discountAmount;
+
+    @Column(nullable = false)
+    private int minAvailableAmount;
+
+    @Column(nullable = false)
+    private LocalDateTime dateIssueStart; // 쿠폰 발급 시간
+
+    @Column(nullable = false)
+    private LocalDateTime dateIssueEnd;
+
+}
---
+++ coupon-core/src/main/java/com/example/couponcore/model/CouponIssue.java
@@ -0,0 +1,38 @@
+package com.example.couponcore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "coupon_issues")
+public class CouponIssue extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long couponId;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime dateIssued;
+
+    @Column(nullable = false)
+    private LocalDateTime dateUsed; // 사용날짜
+
+}
