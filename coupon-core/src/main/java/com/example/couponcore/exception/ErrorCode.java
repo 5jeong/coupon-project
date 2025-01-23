be0b1223067bc66d6463b3d741e09eebe09c8570
5jeong <wjdgns4385@naver.com>
2025-01-23 18:09:42 +0900

    ♻️ [refactor] 오류메시지 변경 및 필요없는 메소드 제거

--- coupon-core/src/main/java/com/example/couponcore/exception/ErrorCode.java
+++ coupon-core/src/main/java/com/example/couponcore/exception/ErrorCode.java
@@ -1,20 +1,17 @@
 package com.example.couponcore.exception;
 
-import lombok.Getter;
-
 public enum ErrorCode {
 
-    INVALID_COUPON_ISSUE_QUANTITY("발급 가능한 수량을 초과 합니다."),
-    INVALID_COUPON_ISSUE_DATE("발급 가능한 일자가 아닙니다."),
-    COUPON_NOT_EXIST("쿠폰이 존재하지 않습니다."),
-    DUPLICATED_COUPON_ISSUE("쿠폰이 이미 발급 되었습니다.");
-    final String message;
+    INVALID_COUPON_ISSUE_QUANTITY("쿠폰 발급 가능한 수량을 유효하지 않습니다."),
+    INVALID_COUPON_ISSUE_DATE("쿠폰 발급 기간이 유효하지 않습니다."),
+    COUPON_NOT_EXIST("존재하지 않는 쿠폰입니다.."),
+    DUPLICATED_COUPON_ISSUE("이미 발급된 쿠폰입니다.");
+
+    public final String message;
 
     ErrorCode(String message) {
         this.message = message;
     }
 
-    public String getMessage() {
-        return message;
-    }
+
 }
