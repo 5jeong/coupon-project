3c13a62dedd4ca35fa530137a5d0e5a746fb4be0
5jeong <wjdgns4385@naver.com>
2025-01-24 20:54:09 +0900

    ✨ [feat] 쿠폰 에러코드 추가

--- coupon-core/src/main/java/com/example/couponcore/exception/ErrorCode.java
+++ coupon-core/src/main/java/com/example/couponcore/exception/ErrorCode.java
@@ -2,7 +2,8 @@ package com.example.couponcore.exception;
 
 public enum ErrorCode {
 
-    INVALID_COUPON_ISSUE_QUANTITY("쿠폰 발급 가능한 수량을 유효하지 않습니다."),
+    FAIL_COUPON_ISSUE_REQUES("쿠폰 발급 요청에 실패하였습니다."),
+    INVALID_COUPON_ISSUE_QUANTITY("쿠폰 발급 가능한 수량이 유효하지 않습니다."),
     INVALID_COUPON_ISSUE_DATE("쿠폰 발급 기간이 유효하지 않습니다."),
     COUPON_NOT_EXIST("존재하지 않는 쿠폰입니다.."),
     DUPLICATED_COUPON_ISSUE("이미 발급된 쿠폰입니다.");
@@ -12,6 +13,5 @@ public enum ErrorCode {
     ErrorCode(String message) {
         this.message = message;
     }
-
 
 }
